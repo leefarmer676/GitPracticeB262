09b5b87428305d22ea635c10fa36613c2e2017d7
leefarmer676 <lee.farmer0208@gmail.com>
2022-07-03 16:33:09 -0600

    Food.java updated work2

--- src/test/java/gitPracticeInstructions/Food.java
+++ src/test/java/gitPracticeInstructions/Food.java
@@ -3,4 +3,7 @@ package gitPracticeInstructions;
 public class Food {
     //work 1
     //work 2
+
+    //Create a pull request to merge the feature-a changes to the develop branch
+    //Checkout to local develop and update it
 }
